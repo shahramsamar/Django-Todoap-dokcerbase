513416c250d78b9720accffa8a6c978120ba2ef8
shahramsamar <shahramsamar2010@gmail.com>
2024-09-23 17:36:23 +0330

    setup setting all app and templates

--- website/urls.py
+++ website/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-
+from website  import views
 
 urlpatterns = [
 
-    path('index',),
+    path('index', views.index, name="index"),
 
 ]
